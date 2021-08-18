b8e11f707f600d6ad8b3308126dd74f1cdf6ddf1
cheofusi <fusibrandon13@gmail.com>
2021-08-18 20:57:45 +0100

    refactor all Playback methods so any calls to the C api can throw. Also add new loop back functionality

--- just_playback/playback.py
+++ just_playback/playback.py
@@ -6,7 +6,8 @@ from typing import Optional, Any
 
 from tinytag import TinyTag
 from _ma_playback import ffi, lib
-from .ma_errs import MA_ERRORS
+from .ma_result import MA_RESULT_STR, MiniaudioError
+
 
 
 class Playback:
@@ -16,21 +17,23 @@ class Playback:
     """
     
     def __init__(self, path_to_file: Optional[str] = ''):
-        if not lib.check_devices():
-            logging.critical('No device is available for playback!!')
+        self.__ma_attrs = ffi.new("Attrs *")
+        
+        self.__bind(lib.check_available_playback_devices(self.__ma_attrs))
+
+        if self.__ma_attrs.num_playback_devices < 1:
+            raise MiniaudioError('No playback device is available!!')
         
         else:
-            self.__ma_attrs = ffi.new("Attrs *")
             lib.init_attrs(self.__ma_attrs)
             
             self.__paused: bool = False
             self.__file_duration: float = 0.0
-            self.__playback_volume: float = 1.0
 
             if path_to_file:
                 self.load_file(path_to_file)   
     
-    def load_file(self, path_to_file: str) -> bool:
+    def load_file(self, path_to_file: str) -> None:
         """
             Loads an audio file using one of the available backends.
             This also kills any active playback
@@ -38,23 +41,19 @@ class Playback:
         Args:
             path_to_file: The absolute or relative path to the audio file
 
-        Returns True if the audio file was successfully loaded and False otherwise
+        Throws a FileNotFoundError if the audio file is not found
         """
 
         audio_file = pathlib.Path(path_to_file)
         if not audio_file.exists() or not path_to_file:
-            logging.error('Audio file not found.')
-            return False
+            raise FileNotFoundError('Audio file not found.')
         
-        self.__log_possible_error(lib.terminate_audio_stream(self.__ma_attrs))
+        self.__bind(lib.terminate_audio_stream(self.__ma_attrs))
 
-        if not lib.load_file(self.__ma_attrs, path_to_file.encode('utf-8')):
-            logging.error('Failed to load ' + path_to_file)
-            self.__log_possible_error(False)
-            return False
+        self.__bind(lib.load_file(self.__ma_attrs, path_to_file.encode('utf-8')))
+        self.__bind(lib.init_audio_stream(self.__ma_attrs))
+        self.__bind(lib.set_device_volume(self.__ma_attrs))
 
-        self.__log_possible_error(lib.init_audio_stream(self.__ma_attrs))
-        self.__log_possible_error(lib.set_device_volume(self.__ma_attrs, self.__playback_volume))
         self.__file_duration = TinyTag.get(path_to_file).duration
         
         return True
@@ -68,23 +67,38 @@ class Playback:
 
         if not self.__ma_attrs.audio_stream_ready:
             logging.error('No audio file has been loaded yet!!')
-            return
         
-        self.stop()
-        self.__log_possible_error(lib.start_audio_stream(self.__ma_attrs))
+        else:
+            if self.active:
+                self.stop()
+
+            else:
+                if self.__ma_attrs.audio_stream_ended_naturally:
+                    # the audio file played to completion meanwhile the audio device
+                    # is still running so we've got to stop it.
+
+                    self.__bind(lib.stop_audio_stream(self.__ma_attrs))
+                    self.__ma_attrs.audio_stream_ended_naturally = False
+
+            self.__ma_attrs.frame_offset = 0
+            self.__ma_attrs.frame_offset_modified = True
+            self.__paused = False
+            self.__bind(lib.start_audio_stream(self.__ma_attrs))
 
     def stop(self) -> None:
         """
-            Stops audio playback. Has no effect if playback is inactive
+            Stops audio playback regardless of whether or not it is paused. 
+            Has no effect if playback is inactive
         """
 
         if self.active:
             if not self.__paused:
-                self.__log_possible_error(lib.stop_audio_stream(self.__ma_attrs))
+                # only stop the audio stream if self.pause() didn't
+                self.__bind(lib.stop_audio_stream(self.__ma_attrs))
+
             self.__ma_attrs.frame_offset = 0
-            self.__ma_attrs.user_seeked = True
             self.__paused = False
-    
+            
     def pause(self) -> None:
         """
             Pauses audio playback. Has no effect if playback is inactive or is
@@ -92,7 +106,7 @@ class Playback:
         """
 
         if self.active and not self.__paused:
-            self.__log_possible_error(lib.stop_audio_stream(self.__ma_attrs))
+            self.__bind(lib.stop_audio_stream(self.__ma_attrs))
             self.__paused = True
 
     def resume(self) -> None:
@@ -103,7 +117,7 @@ class Playback:
 
         if self.active and self.__paused:
             self.__paused = False
-            self.__log_possible_error(lib.start_audio_stream(self.__ma_attrs))
+            self.__bind(lib.start_audio_stream(self.__ma_attrs))
     
     def seek(self, pos: float) -> None:
         """
@@ -111,25 +125,38 @@ class Playback:
             inactive
         
         Args:
-            pos: position in seconds for playback to jump to. It's value is clamped
+            pos: position in seconds for playback to jump to. Its value is clamped
                  to the interval [0, self.duration].
         """
 
         if self.active:
             pos = min(max(pos, 0), self.__file_duration)
-            self.__ma_attrs.frame_offset = math.floor(pos * self.__ma_attrs.sample_rate)
-            self.__ma_attrs.user_seeked = True
+            self.__ma_attrs.frame_offset = math.floor(pos * self.__ma_attrs.decoder.outputSampleRate)
+            self.__ma_attrs.frame_offset_modified = True
     
     def set_volume(self, volume: float) -> None:
         """
-            Sets the volume for the playback device
+            Sets the volume for the playback device. Persists
+            across audio file loads until reset
+
         Args:
             volume: A value in the interval [0, 1].
         """
 
-        self.__playback_volume = min(max(volume, 0), 1)
+        self.__ma_attrs.playback_volume = min(max(volume, 0), 1)
         if self.active:
-            self.__log_possible_error(lib.set_device_volume(self.__ma_attrs, volume))
+            self.__bind(lib.set_device_volume(self.__ma_attrs))
+    
+    def loop_at_end(self, loops_at_end: bool) -> None:
+        """
+            Set playback to automatically restart after playing to completion. 
+            By default this doesn't happen. Persists across audio file loads until reset
+
+        Args:
+            loops_at_end: True if playback should loop, False otherwise
+        """
+
+        self.__ma_attrs.loops_at_end = loops_at_end
     
     @property
     def active(self) -> bool:
@@ -139,21 +166,26 @@ class Playback:
     
         if not self.__ma_attrs.audio_stream_ready:
             return False
-
-        return self.__ma_attrs.playback_active or self.__paused
+        
+        else:
+            return self.__ma_attrs.audio_stream_active or self.__paused
 
     @property
     def curr_pos(self) -> float:
         """
-            Returns playback's current position or offset in seconds from the
-            beginning of audio file.
+            Returns playback's current position/offset in seconds from the
+            beginning of audio file if playback is active or the file has 
+            been loaded and -1 otherwise
         """
 
         if self.active:
-            pos = self.__ma_attrs.frame_offset / self.__ma_attrs.sample_rate
-            return pos 
+            return self.__ma_attrs.frame_offset / self.__ma_attrs.decoder.outputSampleRate
         
-        return 0
+        elif self.__ma_attrs.audio_stream_ready:
+            return 0
+
+        else:        
+            return -1
     
     @property
     def paused(self) -> bool:
@@ -161,6 +193,11 @@ class Playback:
     
     @property
     def duration(self) -> float:
+        """
+            Returns the length in seconds of the audio file, which is 0 if no file has
+            been loaded
+        """
+
         return self.__file_duration
     
     @property
@@ -170,22 +207,22 @@ class Playback:
         """
 
         if self.active:
-            volume = lib.get_device_volume(self.__ma_attrs)
-            if volume == -1:
-                self.__log_possible_error(False)
+            self.__bind(lib.get_device_volume(self.__ma_attrs))
+        
+        return self.__ma_attrs.playback_volume
+    
+    @property
+    def loops_at_end(self) -> bool:
+        return self.__ma_attrs.loops_at_end
 
-            else:
-                self.__playback_volume = volume
-            
-        return self.__playback_volume
-
-    def __log_possible_error(self, result: bool) -> None:
+    def __bind(self, ma_res: int) -> None:
         """ 
-            Internal method for error logging. DO NOT CALL
+            Internal method for checking and throwing possible miniaudio errors. 
+            DO NOT CALL
         """
 
-        if not result:
-            logging.debug('Miniaudio says: {}'.format(MA_ERRORS[self.__ma_attrs.op_errcode]))
+        if ma_res:
+            raise MiniaudioError(MA_RESULT_STR[ma_res])
 
     def __del__(self):
-        self.__log_possible_error(lib.terminate_audio_stream(self.__ma_attrs))
+        self.__bind(lib.terminate_audio_stream(self.__ma_attrs))
